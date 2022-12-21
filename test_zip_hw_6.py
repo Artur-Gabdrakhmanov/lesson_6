9ff8b7b956df4a0aa1e484b9c509eef0e237e152
chevchelios <keks1001@mail.ru>
2022-12-22 04:24:47 +0500

    corrected test_read_and_content_csv, added assert

--- test_zip_hw_6.py
+++ test_zip_hw_6.py
@@ -1,17 +1,14 @@
-import glob
 import os
 from os.path import basename
 from zipfile import ZipFile
 import csv
-import pytest
 from PyPDF2 import PdfReader
 from io import TextIOWrapper
 from openpyxl import load_workbook
 
-
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files')
 path_destination = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources')
-path_zip = os.path.join(path_destination, "artur.zip")
+path_zip = os.path.join(path_destination, "lesson_6_hw.zip")
 
 
 def test_create_archive():
@@ -28,8 +25,9 @@ def test_read_and_content_csv():
         csvfile = csv.reader(TextIOWrapper(csvfile))
         list_csv = []
         for r in csvfile:
-            text = "".join(r).replace(";", " ", 3)
+            text = "".join(r).replace(";", " ")
             list_csv.append(text)
+            assert "11|12|13|14|15|16|17|18|19|20" in list_csv, f"В файле нет этих данных {'11|12|13|14|15|16|17|18|19|20'}"
     zf.close()
 
 
@@ -41,7 +39,7 @@ def test_read_and_content_pdf():
             page = reader.pages[0]
             text = page.extract_text()
             assert "Это мой PDF файл для домашнего задания №6" in text, \
-                    f'Это мой PDF файл для домашнего задания №6'
+                f'В файле нет строки Это мой PDF файл для домашнего задания №6'
         finally:
             os.remove(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'my.pdf'))
             zf.close()
